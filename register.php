4a5eb37bab4c18112c5d61a04e24a11a36fc5930
yulin <yulin90625@gmail.com>
2023-06-15 17:55:13 +0800

    增加password hash機制

--- register.php
+++ register.php
@@ -3,20 +3,21 @@
 
   if ( isset($_POST["MemberID"]) )
 	    $MemberID = $_POST["MemberID"];
-    if ( isset($_POST["password"]) )
-	    $password = $_POST["password"];
-    $link = mysqli_connect("localhost","root","","webproject")
-            or die("無法開啟MySQL資料庫連接!<br/>");
+  if ( isset($_POST["password"]) )
+    // $password = $_POST["password"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+  $link = mysqli_connect("localhost","root","","webproject")
+          or die("無法開啟MySQL資料庫連接!<br/>");
 
-    //送出UTF8編碼的MySQL指令
-    mysqli_query($link, 'SET NAMES utf8'); 
+  //送出UTF8編碼的MySQL指令
+  mysqli_query($link, 'SET NAMES utf8'); 
 
-    //4.SQL 條件式  檢查 條件:拿到資料，帳號有重複 
-    $sql = "SELECT MemberID FROM player WHERE MemberID = '{$MemberID}'";
-    $result = $link->query($sql);
-    $rows = $result->num_rows;
-    //查出有幾筆資料?
-    echo $rows > 0 ? '帳號此帳號已被他人使用，請重新輸入帳號' : '';
+  //4.SQL 條件式  檢查 條件:拿到資料，帳號有重複 
+  $sql = "SELECT MemberID FROM player WHERE MemberID = '{$MemberID}'";
+  $result = $link->query($sql);
+  $rows = $result->num_rows;
+  //查出有幾筆資料?
+  echo $rows > 0 ? '帳號此帳號已被他人使用，請重新輸入帳號' : '';
 
   $sql = "INSERT INTO player (MemberID, password)
   VALUES ('$MemberID', '$password')";
@@ -29,4 +30,3 @@
 //   }
 
   $link->close();
-?>
